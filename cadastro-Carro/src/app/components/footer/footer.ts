e0ae04b718b6ad6281c0d4183be4fc3dafaeaf7d
Willian <williansilvamamede@gmail.com>
2025-12-19 13:10:43 -0300

    closes #2 - Componentes Header e Footer Configurados

---
+++ cadastro-Carro/src/app/components/footer/footer.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-footer',
+  imports: [],
+  templateUrl: './footer.html',
+  styleUrl: './footer.css',
+})
+export class Footer {
+  rodape = 'Desenvolvido por Willian Mamede com o curso de Angular da Abutua.';
+}
